2bc935ee3a268fc666f28beebc0a18b4368c7a18
NikolayIliev61 <1086175@ucn.dk>
2021-02-20 03:05:40 +0100

    Nav-top and bottom-
    The bottom navigation is not finishished as its not centered in the middle of the browser. I tried to make it but i couldn't.

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+document.querySelector('#hamburger-menu').addEventListener('click',function(){
+document.querySelector('#nav-menu').classList.toggle('show')
+});
+
+document.querySelector('#hamburger-menu').addEventListener('click',function(){
+    document.querySelector('#hamburger-menu').classList.toggle('X')
+    });
