a38bd5b8af9a855ba835f06874faa025936db8cb
NikolayIliev61 <1086175@ucn.dk>
2021-02-20 12:48:59 +0100

    Nav working

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,11 @@ document.querySelector('#route-one-info-btn').addEventListener('click', function
         document.querySelector('#route-one-flora-btn').classList.toggle('route-section-selected');
     }
 })
+
+document.querySelector('#hamburger-menu').addEventListener('click',function(){
+    document.querySelector('#nav-menu').classList.toggle('show')
+    });
+    
+    document.querySelector('#hamburger-menu').addEventListener('click',function(){
+        document.querySelector('#hamburger-menu').classList.toggle('X')
+        });
